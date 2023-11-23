571c1596fe6b3e983e89bcc3dbfe50c1b00a8cc0
rohanasif1990 <rohanasif1990@gmail.com>
2023-11-23 12:39:39 +0500

    working on navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
+import LandingPage from "./routes/LandingPage";
+import React from "react";
 const App = () => {
-  return <div className="flex flex-col justify-between items-center">
-    
-  </div>;
+  return (
+    <div className="flex flex-col justify-between items-center">
+      <LandingPage />
+    </div>
+  );
 };
 
 export default App;
--- src/routes/LandingPage.jsx
+++ src/routes/LandingPage.jsx
@@ -1,4 +1,10 @@
+import React from "react";
+import logo from "../assets/logo.png";
 const LandingPage = () => {
-  return <div></div>;
+  return (
+    <div className="flex p-4 bg-slate-600 w-full justify-between">
+      <img src={logo} alt="logo" />
+    </div>
+  );
 };
 export default LandingPage;
