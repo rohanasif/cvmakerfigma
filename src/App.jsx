1003a26ba11228c413cc1d1f0d6562bcb5301ad3
rohanasif1990 <rohanasif1990@gmail.com>
2023-11-30 20:41:31 +0500

    create multiple components and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,26 @@
 import React from "react";
 import LandingPage from "./routes/LandingPage";
 import { Routes, Route } from "react-router-dom";
-import Profile from "./routes/Profile";
+import StepOne from "./routes/StepOne";
+import StepTwo from "./routes/StepTwo";
+import StepThree from "./routes/StepThree";
+import StepFour from "./routes/StepFour";
+import Review from "./routes/Review";
+import AddDetails from "./routes/AddDetails";
+import Resume from "./routes/Resume";
+
 const App = () => {
   return (
     <div className="flex flex-col justify-between items-center">
       <Routes>
         <Route path="/" element={<LandingPage />} />
-        <Route path="/profile" element={<Profile />} />
+        <Route path="/one" element={<StepOne />} />
+        <Route path="/two" element={<StepTwo />} />
+        <Route path="/three" element={<StepThree />} />
+        <Route path="/four" element={<StepFour />} />
+        <Route path="/review" element={<Review />} />
+        <Route path="/add" element={<AddDetails />} />
+        <Route path="/resume" element={<Resume />} />
       </Routes>
     </div>
   );
---
+++ src/components/MainNavBar.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import logo from "../assets/logo.png;";
+import { useSelector } from "react-redux";
+const MainNavBar = () => {
+  const user = useSelector((state) => state.users);
+  console.log(user);
+  return (
+    <div className="flex w-full justify-between">
+      <img src={logo} alt="logo" />
+      <div className="flex items-center gap-4">
+        <img src="" alt="" />
+        <p>{user?.name}</p>
+      </div>
+    </div>
+  );
+};
+
+export default MainNavBar;
